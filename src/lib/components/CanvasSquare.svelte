be834fe1f8640d8d629d870b5a7e5814c9880c11
GiovanniPalladino <giovanni.palladino@mail.polimi.it>
2026-04-08 17:29:48 +0200

    costruiamo canvasSquare, clicksCounter, layout e page

---
+++ src/lib/components/CanvasSquare.svelte
@@ -0,0 +1,52 @@
+<script>
+import { onMount } from "svelte"
+
+    let size = $state(50)
+    let color = $state("#ffe00")
+
+    let canvas
+
+    onMount(() => {
+
+    const context = canvas.getContext("2d")
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    context.fillStyle = color
+    context.fillRect(0, 0, size, size)
+    })
+
+</script>
+
+<h3>canvas square</h3>
+
+<article>
+    <canvas width="100" height="100"></canvas>
+
+    <nav>
+        <label>
+            Size: <input type="range">
+        </label>
+
+                <label>
+            Color: <input type="color">
+        </label>
+    </nav>
+</article>
+
+<style>
+    article { 
+        display: flex;
+        gap: 1em;
+        align-items: end;
+     }
+
+      canvas { 
+        border: solid 1px black;
+     }
+
+      article { 
+        display: flex;
+        gap: 1em;
+        flex-direction: column;
+     }
+</style> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
+<script>
+    import ClicksCounter from "$lib/components/ClicksCounter.svelte"
+    import CanvasSquare from "$lib/components/CanvasSquare.svelte"
+
+    let count = $state(5)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+
+<ClicksCounter bind:initialCount={count}/>
+<p>count from parent is {count} </p>
+
+<CanvasSquare />
+
+<style>
+    h1 {
+        color: deepskyblue;
+    }
+</style>
