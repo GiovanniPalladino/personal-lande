e2460332a85177adb726e61aed6f5e36975bf1c6
GiovanniPalladino <giovanni.palladino@mail.polimi.it>
2026-04-08 17:55:45 +0200

    use #each to show a list of todos

---
+++ src/lib/components/TodoList.svelte
@@ -0,0 +1,15 @@
+<script>
+let todo$ = %state([
+    {title: "Assignment 1", done false}
+    {title: "Assignment 2", done false}
+    {title: "Assignment 3", done false}
+])
+</script>
+
+<h3>My todos</h3>
+
+<ul>
+    {#each todos as todo}
+        <li>(todo.title)</li>
+    {/each}
+</ul>
